34ff941c6a8b4802edcd538416bf5fbea4c39f3e
hch_java <2861061959@qq.com>
2025-06-21 18:46:03 +0800

    feat: add head Layout

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -1,10 +1,39 @@
+<script setup lang="ts"></script>
 <template>
-头部组件11
+  <div class="header-container">
+    <div class="blog-title text-white text-xl">xxx的博客</div>
+    <div class="search-box">
+      <input type="text" placeholder="搜索文章..." />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">    
 </script>
 
 <style>
+.header-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  position: relative;
+}
 
+.blog-title {
+  font-size: 22px;
+  font-weight: bold;
+  margin-right: 300px;
+}
+
+.search-box {
+  margin-left: 50px; /* 控制偏右距离 */
+}
+
+.search-box input {
+  padding: 6px 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+}
 </style>
